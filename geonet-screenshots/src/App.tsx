7d722e0c4e0f73e6f1581f329391324e441fbb27
Valentin Edelsbrunner <v.edelsbrunner@protonmail.com>
2023-12-15 15:32:46 +0100

    Initial commit

---
+++ geonet-screenshots/src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css';
+import { Box, Image, VStack, Heading, Container } from '@chakra-ui/react';
+
+function App() {
+  const images = ['01.png', '02.png', '03.png', '04.png', '05.png', '07.png'];
+
+  return (
+    <Container maxW="container.xl" p={4}>
+      <VStack spacing={8}>
+        <Heading as="h1" size="xl">GeoNet</Heading>
+        {images.map((img, index) => (
+          <Box key={index} boxShadow="md" borderRadius="lg" overflow="hidden">
+            <Image 
+              src={`/public/${img}`} 
+              alt={`Image ${index + 1}`} 
+              w={["80%"]} // Responsive width
+              h="auto" 
+              objectFit="contain" // Change to 'contain' to avoid cropping
+            />
+          </Box>
+        ))}
+      </VStack>
+    </Container>
+  );
+}
+
+export default App;
